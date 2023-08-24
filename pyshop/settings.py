c9976bd9911e1bed588cf49e0207d83264a905db
MossaCobra <jaydengalea11@gmail.com>
2023-08-24 14:27:06 +1000

    Update settings.py

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = decouple.config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = decouple.config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['mossacobra.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -121,8 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Define the directories where static files are stored in each app
 STATICFILES_DIRS = [
